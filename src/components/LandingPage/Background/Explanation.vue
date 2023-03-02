34c7bea90e54b42d45349af8615efceadb3db15e
gyuudon3187 <ddahlb@kth.se>
2023-03-02 20:43:35 +0100

    Added i18n and transition on scroll + refactored

--- src/components/LandingPage/Background/Explanation.vue
+++ src/components/LandingPage/Background/Explanation.vue
@@ -1,53 +1,126 @@
 <script setup lang="ts">
+import type { Ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
 const { t } = useI18n();
 
-const basePath = "landingPage.explanation.";
-const assets = "src/assets/";
-
-interface explanation {
-  header: string,
-  description: string,
-  imgSrc?: string
+interface MainHeader {
+  text: string,
+  isVisible: Ref<boolean>
 };
 
-const explanations: explanation[] = [
-  {header: basePath + "findClub.header", 
-  description: t(basePath + "findClub.description"), 
-  imgSrc: assets + "findClub.png"},
+interface Explanation {
+  header: string,
+  description: string,
+  imgSrc: string,
+  isVisible: Ref<boolean>
+};
 
-  {header: basePath + "readingList.header",
-  description: t(basePath + "readingList.description"),
-  imgSrc: assets + "readingList.png"},
+interface Offset {
+  viewport: number,
+  mainHeader: number
+};
 
-  {header: basePath + "synopsisAndProgress.header",
-  description: t(basePath + "synopsisAndProgress.description"),
-  imgSrc: assets + "book.png"},
+function initializeVariables() {
+  const basePath = "landingPage.explanation.";
 
-  {header: basePath + "voteResults.header",
-  description: t(basePath + "voteResults.description"),
-  imgSrc: assets + "voteResults.png"},
-]
+  function initializeMainHeader(): MainHeader {
+    return {
+      text: basePath + "header",
+      isVisible: ref(false)
+    }
+  }
+
+  function initializeExplanations(explanationNames: string[]): Explanation[] {
+    const assetsPath = "src/assets/";
+    const initializedExplanations: Explanation[] = [];
+
+    explanationNames.forEach(name => {
+      initializedExplanations.push({
+        header: basePath + name + ".header",
+        description: t(basePath + name + ".description"),
+        imgSrc: assetsPath + name + ".png",
+        isVisible: ref(false)
+      })
+    })
+
+    return initializedExplanations;
+  }
+
+  function initializeOffset(): Offset {
+    return {
+      viewport: 600,
+      mainHeader: 120
+    }
+  }
+
+  return {
+    mainHeader: initializeMainHeader(),
+    explanations: initializeExplanations(["findClub", "readingList", "synopsisAndProgress", "voteResults"]),
+    offset: initializeOffset()
+  }
+}
+
+function leftOrRight(name: string, index: number): string {
+  return name + (index % 2 === 0 ? '-left' : '-right');
+}
+
+const {mainHeader, explanations, offset} = initializeVariables();
+let observedPositions: number[] = [];
+
+onMounted(() => {
+  function eventListener(): void {
+    let ticking = false;
+    let i = 0;
+
+    if(!ticking) {
+      window.requestAnimationFrame(() => {
+        const observedViewport = window.scrollY + window.innerHeight - offset.viewport;
+        mainHeader.isVisible.value = observedViewport > offset.mainHeader;
+        observedPositions.forEach(observedPos => {
+          explanations[i++].isVisible.value = observedViewport > observedPos;
+        })
+      })
+    }
+
+    ticking = true;
+  }
+
+  const explanationLevelCount = Math.ceil(document.querySelector("#explanation-items")!.children.length) / 2;
+  for(let i = 0; i < explanationLevelCount; i++) {
+    observedPositions.push((i * 400) + 350)
+    observedPositions.push((i * 400) + 350)
+  }
+  window.addEventListener("scroll", eventListener)
+})
 </script>
 
 <template>
   <div class="explanation">
-    <h1 class="how-it-works-header">{{ t("landingPage.explanation.header") }}</h1>
-  
-    <div class="explanation-items">
 
-      <div v-for="(explanation, index) in explanations" :key="index" class="explanation-item">
-        <h1 :class="index % 2 == 0 ? 'explanation-header-left' : 'explanation-header-right'">
-          {{ t(explanation.header) }}
-        </h1>
-        <p v-html="explanation.description" />
-        <div class="explanation-image">
-          <img
-          style="height: 100%"
-          :src="explanation.imgSrc"
-        />
+    <Transition name="slide-left" mode="out-in">
+      <h1 id="how-it-works" class="explanation-header-right" v-show="mainHeader.isVisible.value">{{ t(mainHeader.text) }}</h1>
+    </Transition>
+  
+    <div id="explanation-items">
+
+      <Transition 
+        v-for="(explanation, index) in explanations" 
+        :key="index"
+        :name="leftOrRight('slide', index)">
+
+        <div class="explanation-item" v-show="explanation.isVisible.value">
+          <h1 :class="leftOrRight('explanation-header', index)">{{ t(explanation.header) }}</h1>
+
+          <p v-html="explanation.description" />
+
+          <div class="explanation-image">
+            <img style="height: 100%" :src="explanation.imgSrc"/>
+          </div>
         </div>
-      </div>
+
+      </Transition>
+      
 
     </div>
   </div>
@@ -60,7 +133,7 @@ const explanations: explanation[] = [
   width: 100%;
 }
 
-.explanation-items {
+#explanation-items {
   display: grid;
   grid-template-columns: repeat(2, 500px);
   grid-auto-rows: 350px;
@@ -87,19 +160,15 @@ const explanations: explanation[] = [
   height: 200px;
 }
 
-.how-it-works-header {
+#how-it-works {
   position: absolute;
   top: -200px;
   right: 0px;
   font-weight: 400;
   font-size: 96px;
-  background: linear-gradient(270deg, #FC4F59 22.01%,  #FC984F 99.32%);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  background-clip: text;
 }
 
-.how-it-works-header::after {
+#how-it-works::after {
   content: '';
   display: block;
   position: absolute;
@@ -112,5 +181,51 @@ const explanations: explanation[] = [
 
 .explanation-item p {
   padding-bottom: 1rem;
+}
+
+.slide-left-enter-from {
+  opacity: 0;
+}
+
+.slide-left-enter-active {
+  transition: all 0.5s ease-out;
+  transform: translateX(-50px);
+}
+
+.slide-left-enter-to {
+  opacity: 1;
+  transform: translateX(0px);
+}
+
+.slide-left-leave-to {
+  opacity: 0;
+}
+
+.slide-left-leave-active {
+  transition: all 0.5s ease-in;
+  transform: translateX(-50px);
+}
+
+.slide-right-enter-from {
+  opacity: 0;
+}
+
+.slide-right-enter-active {
+  transition: all 0.5s ease-out;
+  transform: translateX(50px);
+}
+
+.slide-right-enter-to {
+  opacity: 1;
+  transform: translateX(0px);
+}
+
+.slide-right-leave-to {
+  opacity: 0;
+}
+
+.slide-right-leave-active {
+  transition: all 0.5s ease-in;
+  transform: translateX(50px);
 }
 </style>
